52cf67c130cc1d5d336b653182d5ca29e7b63ac4
Vetorlog 020 <wagner@vetorlog.com>
2019-03-29 10:00:20 -0300

    'projeto'

---
+++ src/services/api.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class ApiService {
+
+    constructor(private httpService: HttpClient) { }
+
+    /**
+     * Metódo responsável pelo retorno dos dados arquimedes
+     * 
+     * @return `Observable` como provider do retorno dos dados
+     */  
+    public get() {
+    let url = "https://api.emeter.com.br:8443/emeter/api/v1/arquimedes/dados";
+
+    const header = new HttpHeaders({
+        'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Accept-Language': 'pt_BR',
+      'Authorization': 'ZFF3NHc5V2dYY1E='
+    });
+
+    let option = {
+        headers: header
+    };
+    
+    return this.httpService.get(url, option);
+    }
+}
