59504799b26fda3a5d1ff79b460e336b506174eb
Allan Maeda <allanmaeda@gmail.com>
2019-04-03 00:41:27 -0300

    add bootstrap to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiService } from 'src/services/api.service';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CarouselComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    MDBBootstrapModule.forRoot(),
     AppRoutingModule,
     HttpClientModule
-  ],
+  ], schemas: [ NO_ERRORS_SCHEMA ],
   providers: [ApiService],
   bootstrap: [AppComponent]
 })
